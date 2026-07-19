7d836c0aa43dd17e7a79244fa7b462c2fcee40ec
dev.geonwoo_ <woo6629058@catholic.ac.kr>
2026-07-19 17:55:35 +0900

    [Fix]#31 배송지 입력 검증 정규식 개선

--- src/main/java/com/redo/domain/reward/dto/req/ShippingAddressCreateRequestDTO.java
+++ src/main/java/com/redo/domain/reward/dto/req/ShippingAddressCreateRequestDTO.java
@@ -12,12 +12,12 @@ public record ShippingAddressCreateRequestDTO(
 
         @NotBlank(message = "받는 이를 입력해주세요.")
         @Size(max = 10, message = "받는 이는 10자 이하로 입력해주세요.")
-        @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "받는 이는 문자만 입력해주세요.")
+        @Pattern(regexp = "^[가-힣a-zA-Z ]+$", message = "받는 이는 문자만 입력해주세요.")
         String receiverName,
 
         @NotBlank(message = "연락처를 입력해주세요.")
         @Pattern(
-                regexp = "^\\d{3}-?\\d{4}-?\\d{4}$",
+                regexp = "^(\\d{11}|\\d{3}-\\d{4}-\\d{4})$",
                 message = "연락처는 숫자 11자리 또는 000-0000-0000 형식으로 입력해주세요."
         )
         String phone,
