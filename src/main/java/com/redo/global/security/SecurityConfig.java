d1910d7b93b333088e5e217f2844ec890bd77df1
JINHA CHEON <jinha1665@gmail.com>
2026-07-21 19:47:09 +0900

    [Fix]#67 CORS 로컬/배포 환경 동시 허용 + Swagger 서버 목록 추가

--- src/main/java/com/redo/global/security/SecurityConfig.java
+++ src/main/java/com/redo/global/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(baseUrl));
+        configuration.setAllowedOrigins(List.of(baseUrl, "http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
